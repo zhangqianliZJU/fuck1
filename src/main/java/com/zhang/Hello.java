4efa9fca90d76f9f8bb45025474888cee5a59acb
zhangqianli <zhangqianli414@163.com>
2018-04-04 22:40:15 +0800

    Modify Hello

--- src/main/java/com/zhang/Hello.java
+++ src/main/java/com/zhang/Hello.java
@@ -1,6 +1,9 @@
 package com.zhang;
 
 public class Hello {
-  听说不一定hello world
-    System.out.println("Hello world!");
+  //听说不一定hello world
+  public static void main(String[] args) {
+
+      System.out.println("Hello world!");
+  }
 }
